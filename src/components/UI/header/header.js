6be48e385c5b2d5d61175e86dbcb2c8d7e0a7eea
isaaccodekill <isaacbello3200@gmail.com>
2019-11-13 08:55:37 +1345

    created header component

---
+++ src/components/UI/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './header.module.css'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as LogoutImage } from '../../../assets/images/logout.svg'
+
+
+const Header = () => {
+    return (
+        <div className={styles.Header}>
+            <h2 className={styles.Logo}>Venues App</h2>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/Logout"><LogoutImage/> Logout</NavLink>
+        </div>
+    )
+}
+
+export default Header
